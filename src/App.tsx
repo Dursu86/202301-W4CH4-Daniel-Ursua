f8f05a249506618e16edb1d49f7c9e36fe17e40b
Daniel <dursuaf@gmail.com>
2023-02-09 21:14:04 +0100

    Update estructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Display } from "./features/components/display/display";
 import { Keyboard } from "./features/components/keyboard/keyboard";
 
 function App() {
   return (
     <div className="App">
+      <Display></Display>
       <Keyboard></Keyboard>
     </div>
   );
---
+++ src/features/components/actions/actions.tsx
@@ -0,0 +1,13 @@
+import { Display } from "../display/display";
+import { Action } from "./action/action";
+
+export function Actions() {
+  return (
+    <>
+      <div className="actions">
+        <Display></Display>
+        <Action></Action>
+      </div>
+    </>
+  );
+}
