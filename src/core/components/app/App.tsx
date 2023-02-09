8f2183af68c95b1f7533be61b728f18862938734
Daniel <dursuaf@gmail.com>
2023-02-09 21:35:36 +0100

    Fix index problems

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,17 @@
+import { Actions } from "../../../features/components/actions/actions";
+import { Info } from "../../../features/components/info/info";
+import { Keyboard } from "../../../features/components/keyboard/keyboard";
+
+function App() {
+  return (
+    <>
+      <Info></Info>
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
+    </>
+  );
+}
+
+export default App;
